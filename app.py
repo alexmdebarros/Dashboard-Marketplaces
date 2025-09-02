58bccf33068b2d83e8c1cf814b6cfd3a619dcfe0
alexmdebarros <alexmdebarros@gmail.com>
2025-09-02 13:47:17 -0300

    fix

--- app.py
+++ app.py
@@ -57,23 +57,9 @@ SCOPES = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive",
 ]
-
-# Monta as credenciais do jeito certo, lendo cada parte do segredo
-# e corrigindo a formatação da chave privada.
-creds_dict = {
-    "type": st.secrets.google_service_account.type,
-    "project_id": st.secrets.google_service_account.project_id,
-    "private_key_id": st.secrets.google_service_account.private_key_id,
-    "private_key": st.secrets.google_service_account.private_key.replace('\\n', '\n'),
-    "client_email": st.secrets.google_service_account.client_email,
-    "client_id": st.secrets.google_service_account.client_id,
-    "auth_uri": st.secrets.google_service_account.auth_uri,
-    "token_uri": st.secrets.google_service_account.token_uri,
-    "auth_provider_x509_cert_url": st.secrets.google_service_account.auth_provider_x509_cert_url,
-    "client_x509_cert_url": st.secrets.google_service_account.client_x509_cert_url
-}
-creds = Credentials.from_service_account_info(creds_dict, scopes=SCOPES)
-
+creds = Credentials.from_service_account_info(
+    st.secrets["google_service_account"], scopes=SCOPES
+)
 gc = gspread.authorize(creds)
 ws = gc.open_by_key(SHEET_KEY).worksheet("Dados")
 header = ws.row_values(1)
